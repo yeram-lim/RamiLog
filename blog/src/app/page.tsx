4e67def295397a01b135242511c3177f49700d0f
LimYeram <yeramyeye@gmail.com>
2023-09-29 01:33:23 +0900

    😺[CHORE] add post component in home page (#3)

--- blog/src/app/page.tsx
+++ blog/src/app/page.tsx
@@ -1,5 +1,17 @@
 import Image from "next/image";
+import Post from "../../components/Post";
 
 export default function Home() {
-  return <article>HOME</article>;
+  return (
+    <>
+      <section>
+        <h1>Blog.</h1>
+        <div>
+          <Post imgSrc={""} title={"첫 게시물"} />
+        </div>
+      </section>
+      <section></section>
+      <section></section>
+    </>
+  );
 }
