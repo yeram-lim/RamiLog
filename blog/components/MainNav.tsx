1791f8489ad05b1465f15888231c616509ea48e9
LimYeram <yeramyeye@gmail.com>
2023-09-28 20:34:18 +0900

    🍭[STYLE] nav (#2)

--- blog/components/MainNav.tsx
+++ blog/components/MainNav.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
@@ -11,10 +12,19 @@ const navlinks: { title: string; link: string }[] = [
 const MainNav = () => {
   return (
     <nav className="flex justify-between py-2 px-8 h-16 items-center bg-pink-100">
-      <div className="flex gap-6">
+      <div className="flex gap-6 items-center">
         {navlinks.map((nav) => (
           <Link href={nav.link} key={nav.title}>
-            {nav.title}
+            {nav.title === "Home" ? (
+              <Image
+                src={`/logo.png`}
+                width={30}
+                height={30}
+                alt={"logo"}
+              ></Image>
+            ) : (
+              nav.title
+            )}
           </Link>
         ))}
       </div>
