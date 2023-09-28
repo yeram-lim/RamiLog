689ecf1ebcd38513aa5405fafdbbd520fe01e037
LimYeram <yeramyeye@gmail.com>
2023-09-28 20:25:22 +0900

    🍀[FEAT] 기본 페이지 추가(#2)

---
+++ blog/src/app/blog/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const BlogPage = () => {
+  return <article>BlogPage</article>;
+};
+
+export default BlogPage;
---
+++ blog/src/app/time-line/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const TimeLinePage = () => {
+  return <article>TimeLinePage</article>;
+};
+
+export default TimeLinePage;
