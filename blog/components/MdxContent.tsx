f42a67e86808df90d97de0309bf21462819abf6c
LimYeram <yeramyeye@gmail.com>
2023-10-08 15:32:18 +0900

    😺[CHORE] 커스텀 컴포넌트 넣을 수 있음 (#4)

--- blog/components/MdxContent.tsx
+++ blog/components/MdxContent.tsx
@@ -6,6 +6,26 @@ type MdxContentProps = {
   source: MDXRemoteSerializeResult;
 };
 
+/** Place your custom MDX components here */
+const MdxComponents = {
+  /** h1 colored in yellow */
+  h1: (props: React.HTMLProps<HTMLHeadingElement>) => (
+    <h1 style={{ color: "#FFF676" }} {...props} />
+  ),
+  /** Card component */
+  Card: (props: React.HTMLProps<HTMLDivElement>) => (
+    <div
+      style={{
+        background: "#333",
+        borderRadius: "0.25rem",
+        padding: "0.5rem 1rem",
+        color: "white",
+      }}
+      {...props}
+    />
+  ),
+};
+
 export function MdxContent({ source }: MdxContentProps) {
-  return <MDXRemote {...source} />;
+  return <MDXRemote {...source} components={MdxComponents} />;
 }
