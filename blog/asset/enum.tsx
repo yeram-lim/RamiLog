6726dc3566f13ef458c305675ef1046e65e69ba6
yeram-lim <yeramyeye@gmail.com>
2024-12-29 23:02:13 +0900

    😺[CHORE] tag 정리 -> 카멜 케이스로 정리 (#3)

--- blog/asset/enum.tsx
+++ blog/asset/enum.tsx
@@ -1,15 +1,20 @@
 export enum PostTag {
   // 기술 관련
-  NEXT_JS = "Next.js",
-  REACT_JS = "React.js",
-  TYPESCRIPT = "Typescript",
-  JAVASCRIPT = "Javascript",
+  // 상위 카테고리
   FRONT_END = "FrontEnd",
+  // 하위 카테고리
+  NEXT_JS = "NextJs",
+  REACT_JS = "ReactJs",
+  TYPESCRIPT = "TypeScript",
+  JAVASCRIPT = "JavaScript",
 
   // 알고리즘
+  // 상위 카테고리
   ALGORITHM = "Algorithm",
+  // 하위 카테고리
+  DYNAMIC_PROGRAMMING = "DynamicProgramming",
 
   // 프로젝트 관련
   PROJECT = "Project",
-  PROJECT_BLOG = "ProjectBlog",
+  BLOG = "Blog",
 }
